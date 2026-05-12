4f72ff363637f5b72408b0a2f17d07ece3ab0c20
Barnaby K. Gichana <gichana@andrews.edu>
2026-05-12 09:28:12 -0500

    add: solution 01-02

---
+++ syllabus/lesson01/exercises/02.sh
@@ -0,0 +1,15 @@
+ls -l /
+
+echo -e "  \n
+The -l flag enables ls's long listing format
+The first 10 characters of each line provide metadata for their respective files \n
+
+Breakdown by line:
+
+1     -> File Type
+2-4   -> Owner permissions
+5-7   -> Group permissions
+8-10  -> Others permissions \n
+
+Read more here: https://www.gnu.org/software/coreutils/manual/html_node/What-information-is-listed.html \n
+"
